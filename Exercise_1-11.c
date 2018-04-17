125ff17689fdd61a74fe3ac29ade1d6fb57267c9
paulenright <paulenright@creativityincorporated.com>
2018-04-17 17:51:36 -0400

    Adding Exercise 1-11.

---
+++ Exercise_1-11.c
@@ -0,0 +1,7 @@
+Test cases for word count program:
+- Test words separated by spaces
+- Test words separated by tabs
+- Test words separated by newlines
+- Test multiple spaces/tabs/newlines in succession
+- Test very long words
+- Test entering special characters
